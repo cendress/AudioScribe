434ae40af7c0608fe333ddcfaa55918a05dddc15
cendress <95492688+cendress@users.noreply.github.com>
2025-07-21 19:28:33 -0400

    Display a simple progress bar to represent transcription progress at the top of RootView

--- AudioScribe/UI/RootView.swift
+++ AudioScribe/UI/RootView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct RootView: View {
+    @SwiftUI.State private var globalProgress: Double = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .onReceive(TranscriptionManager.shared.progressPublisher) {
+            globalProgress = $0
+        }
+        .overlay(alignment: .top) {
+            if globalProgress > 0 && globalProgress < 1 {
+                ProgressView(value: globalProgress).progressViewStyle(.linear)
+                    .frame(height: 2)
+            }
+        }
     }
 }
 
