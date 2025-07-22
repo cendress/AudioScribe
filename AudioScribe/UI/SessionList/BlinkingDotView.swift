71069f22c21d270ee6a77b3272e46d0184c29cdd
cendress <95492688+cendress@users.noreply.github.com>
2025-07-22 13:33:41 -0400

    Create a BlinkingDotView that blinks forever when visible

---
+++ AudioScribe/UI/SessionList/BlinkingDotView.swift
@@ -0,0 +1,31 @@
+//
+//  BlinkingDotView.swift
+//  AudioScribe
+//
+//  Created by Christopher Endress on 7/22/25.
+//
+
+import SwiftUI
+
+struct BlinkingDotView: View {
+    @SwiftUI.State private var visible = false
+    
+    var body: some View {
+        Circle()
+            .fill(Color.white)
+            .frame(width: 8, height: 8)
+            .opacity(visible ? 1 : 0)
+            .onAppear {
+                withAnimation(
+                    .linear(duration: 0.6)
+                    .repeatForever(autoreverses: true)
+                ) {
+                    visible = true
+                }
+            }
+    }
+}
+
+#Preview {
+    BlinkingDotView()
+}
