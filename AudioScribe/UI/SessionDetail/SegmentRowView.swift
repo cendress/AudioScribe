2545c10603563d63d042af91eae501d93264895a
cendress <95492688+cendress@users.noreply.github.com>
2025-07-22 14:09:41 -0400

    Create SegmentRowView

---
+++ AudioScribe/UI/SessionDetail/SegmentRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SegmentRowView.swift
+//  AudioScribe
+//
+//  Created by Christopher Endress on 7/22/25.
+//
+
+import SwiftUI
+
+struct SegmentRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SegmentRowView()
+}
