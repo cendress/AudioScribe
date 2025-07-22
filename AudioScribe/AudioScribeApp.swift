aed62b1609ae34377f446c9aa6d992e1a43549f8
cendress <95492688+cendress@users.noreply.github.com>
2025-07-22 13:56:26 -0400

    Inject the DataController context into the root of the app

--- AudioScribe/AudioScribeApp.swift
+++ AudioScribe/AudioScribeApp.swift
@@ -10,36 +10,34 @@ import SwiftUI
 
 @main
 struct AudioScribeApp: App {
-  init() {
-    #if DEBUG
-    let defaults = UserDefaults.standard
-    // Only run the save block on the very first launch
-    if !defaults.bool(forKey: "didSaveOpenAIToken") {
-      do {
-          try KeychainStore.shared.save(
-              value: Config.openAIToken,  
-              key: "OPENAI_TOKEN"
-          )
-          
-        defaults.set(true, forKey: "didSaveOpenAIToken")
-      } catch {
-        print("Keychain save failed:", error)
-      }
+    let dataController = DataController.shared
+    
+    init() {
+#if DEBUG
+        let defaults = UserDefaults.standard
+        // Only run the save block on the very first launch
+        if !defaults.bool(forKey: "didSaveOpenAIToken") {
+            do {
+                try KeychainStore.shared.save(
+                    value: Config.openAIToken,
+                    key: "OPENAI_TOKEN"
+                )
+                
+                defaults.set(true, forKey: "didSaveOpenAIToken")
+            } catch {
+                print("Keychain save failed:", error)
+            }
+        }
+#endif
     }
-    #endif
-  }
     
     //MARK: - Swift data
     
-  var body: some Scene {
-    WindowGroup {
-      RootView()
-        .modelContainer(for: [
-          RecordingSession.self,
-          Segment.self,
-          Transcription.self
-        ])
-        .preferredColorScheme(.dark)
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .environment(\.modelContext, DataController.shared.container.mainContext)
+                .preferredColorScheme(.dark)
+        }
     }
-  }
 }
