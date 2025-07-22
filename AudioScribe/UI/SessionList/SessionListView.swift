38658c9b652a6eb423751af3a6fbf6a2bc1d6d8e
cendress <95492688+cendress@users.noreply.github.com>
2025-07-22 13:24:42 -0400

    For simplicity, only retain the 'Done' and 'Any' statuses in the menu

--- AudioScribe/UI/SessionList/SessionListView.swift
+++ AudioScribe/UI/SessionList/SessionListView.swift
@@ -86,9 +86,7 @@ struct SessionListView: View {
                             Text("Any")
                                 .tag(Transcription.Status?.none)
                             
-                            ForEach([Transcription.Status.pending,
-                                     .done,
-                                     .failed], id: \.self) { status in
+                            ForEach([Transcription.Status.done], id: \.self) { status in
                                          Text(status.name)
                                              .tag(Transcription.Status?.some(status))
                                      }
